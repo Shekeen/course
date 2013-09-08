0cb948427fdd403fc59002393c3173e40cec4807
Anton Guryanov <guryanov91@gmail.com>
2013-09-08 20:53:42 +0400

    Structure/BKTree almost completed

--- src/Structure/BKTree.hs
+++ src/Structure/BKTree.hs
@@ -16,119 +16,82 @@ module Structure.BKTree
 import Structure.MetricSpace
 import Data.Map (Map)
 import qualified Data.Map as M
-import Prelude hiding (any, foldr)
+import Prelude hiding (any, foldr, foldl)
 import Data.Foldable
 import Data.Monoid
 
-data BKTree a =
-  Node a !Int (BMap a)
-  | Leaf
+data BKTree a = Node a !Int (BMap a)
+              | Leaf
   deriving (Eq, Show)
 
-empty ::
-  BKTree a
-empty =
-  error "todo"
+empty :: BKTree a
+empty = Leaf
+
 
 instance MetricSpace a => Monoid (BKTree a) where
-  mempty =
-    error "todo"
-  mappend =
-    error "todo"
-  mconcat =
-    error "todo"
+  mempty = empty
+  mappend a b = foldl' (flip (.:.)) a (asList b)
+  mconcat = foldl' mappend empty
 
 instance Foldable BKTree where
-  foldl =
-    error "todo"
-  foldr =
-    error "todo"
+  foldl f z = foldl' f z . asList
+  foldr f z = foldr f z . asList
 
-bktree ::
-  (MetricSpace a, Foldable f) =>
-  f a
-  -> BKTree a
-bktree =
-  error "todo"
+bktree :: (MetricSpace a, Foldable f) => f a -> BKTree a
+bktree = foldl (flip (.:.)) empty
 
-isEmpty ::
-  BKTree a
-  -> Bool
-isEmpty =
-  error "todo"
 
-size ::
-  BKTree a
-  -> Int
-size =
-  error "todo"
+isEmpty :: BKTree a -> Bool
+isEmpty Leaf = True
+isEmpty _ = False
 
-(.:.) ::
-  MetricSpace a =>
-  a
-  -> BKTree a
-  -> BKTree a
-(.:.) =
-  error "todo"
+
+size :: BKTree a -> Int
+size Leaf = 0
+size (Node _ s _) = s
+
+
+(.:.) :: MetricSpace a => a -> BKTree a -> BKTree a
+el .:. Leaf = Node el 1 M.empty
+el .:. Node z s m = Node z (s+1) (M.alter (\zz -> Just $! case zz of
+                                                            Just w -> el .:. w
+                                                            Nothing -> Node el 1 M.empty)
+                                          (el <--> z)
+                                          m)
+
 
 infixr 5 .:.
 
-member ::
-  MetricSpace a =>
-  a
-  -> BKTree a
-  -> Bool
-member =
-  error "todo"
+member :: MetricSpace a => a -> BKTree a -> Bool
+member _ Leaf = False
+member el (Node z _ m) = d == 0 || any (member el) (M.lookup d m)
+  where
+    d = el <--> z
 
-withinDistance ::
-  MetricSpace a =>
-  Int
-  -> a
-  -> BKTree a
-  -> [(Int, a)]
-withinDistance =
-  error "todo"
 
-fromWords ::
-  String
-  -> BKTree String
-fromWords =
-  error "todo"
+withinDistance :: MetricSpace a => Int -> a -> BKTree a -> [(Int, a)]
+withinDistance = undefined
 
-fromDictionaryFile ::
-  FilePath
-  -> IO (BKTree String)
-fromDictionaryFile =
-  error "todo"
+
+fromWords :: String -> BKTree String
+fromWords = bktree . words
+
+
+fromDictionaryFile :: FilePath -> IO (BKTree String)
+fromDictionaryFile = fmap fromWords . readFile
+
 
 -- not exported
 
-type BMap a =
-  Map Int (BKTree a)
+type BMap a = Map Int (BKTree a)
 
-asList ::
-  BKTree a
-  -> [a]
-asList Leaf =
-  []
-asList (Node a _ m) =
-  a : (fmap snd (M.toList m) >>= asList)
+asList :: BKTree a -> [a]
+asList Leaf = []
+asList (Node a _ m) = a : (fmap snd (M.toList m) >>= asList)
 
-usingMap ::
-  (BMap a -> x)
-  -> x
-  -> BKTree a
-  -> x
-usingMap _ l Leaf =
-  l
-usingMap f _ (Node _ _ m) =
-  f m
+usingMap :: (BMap a -> x) -> x -> BKTree a -> x
+usingMap _ l Leaf = l
+usingMap f _ (Node _ _ m) = f m
 
-breakMap ::
-  Int
-  -> Int
-  -> BMap a
-  -> BMap a
-breakMap d n m =
-  fst $ M.split (d + n + 1) (snd $ M.split (d - n - 1) m)
+breakMap :: Int -> Int -> BMap a -> BMap a
+breakMap d n m = fst $ M.split (d + n + 1) (snd $ M.split (d - n - 1) m)
